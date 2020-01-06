3a0bc41578d95100ae0e355f097824d41c235212
Michal Malecki <high.flight.mm@gmail.com>
2020-01-06 23:18:03 +0100

    all

---
+++ src/main/java/com/example/bosch/BoschApplication.java
@@ -0,0 +1,13 @@
+package com.example.bosch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschApplication.class, args);
+	}
+
+}
